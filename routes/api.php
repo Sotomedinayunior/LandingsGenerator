14759cd65c729d647739fc50810a579aaf586673
Sotomedinayunior <39248241+Sotomedinayunior@users.noreply.github.com>
2024-08-12 10:39:32 -0400

    Correcion del controller landing

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\LandingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +19,11 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->get('/protected', function () {
     return response()->json(['message' => 'You are authenticated!']);
+});
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/landing', [LandingController::class, 'create']);
+    Route::get('/landing/{id}', [LandingController::class, 'index']);
+    Route::delete('/landing/{id}', [LandingController::class, 'destroy']);
+    // Añade otras rutas protegidas aquí
 });
